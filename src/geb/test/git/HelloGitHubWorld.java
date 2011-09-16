a9de9a0f5a933c2b6d070b70711e29e7dee228a9
Geb geb <geb@fry.com>
2011-09-16 14:26:45 -0400

    modified the comment

--- src/geb/test/git/HelloGitHubWorld.java
+++ src/geb/test/git/HelloGitHubWorld.java
@@ -3,11 +3,11 @@ package geb.test.git;
 public class HelloGitHubWorld {
 
 	/**
-	 * testing uploading of a project tp Github
+	 * testing uploading of a project to Github
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hola there, Githubbies. lol");
-
+		
 	}
 
 }
