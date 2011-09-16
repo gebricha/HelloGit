ed0d71c5f51aa16d1f53db4fe94afc0bbaab57f1
Geb geb <geb@fry.com>
2011-09-16 14:17:07 -0400

    First hola github commit

---
+++ src/geb/test/git/HelloGitHubWorld.java
@@ -0,0 +1,13 @@
+package geb.test.git;
+
+public class HelloGitHubWorld {
+
+	/**
+	 * testing uploading of a project tp Github
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola there, Githubbies. lol");
+
+	}
+
+}
